5ba38c21b627bf52be80d03c4519f03733da6f59
Jose Castagnino <jose@muttdata.ai>
2024-07-28 15:56:04 -0300

    fix first quarter

--- imgalaxy/lens_annotation.py
+++ imgalaxy/lens_annotation.py
@@ -1,5 +1,4 @@
 """Annotate simulated strong gravitational lenses images."""
-from datetime import datetime
 from pathlib import Path
 from typing import List, Tuple
 
@@ -14,10 +13,8 @@ from photutils.segmentation import (
     SourceFinder,
     make_2dgaussian_kernel,
 )
-from skimage.draw import polygon2mask  # pylint: disable=no-name-in-module
-from skimage.measure import find_contours  # pylint: disable=no-name-in-module
 
-from imgalaxy.cfg import DES_DATA, DES_NO_SOURCE_DATA, LENSING_MASKS_DIR
+from imgalaxy.cfg import DES_NO_SOURCE_DATA, LENSING_MASKS_DIR
 
 
 def has_center(segmentation: NDArray, label: int) -> bool:
@@ -37,7 +34,7 @@ def remove_center_label(segmentation_map: SegmentationImage) -> NDArray:
     for label in segmentation_map.labels:
         if has_center(mask, label):
             mask[mask == label] = 0
-    return mask > 0
+    return 1 * (mask > 0)
 
 
 def keep_only_center(segmentation_map: SegmentationImage) -> NDArray:
@@ -80,17 +77,23 @@ def segment_background(
     return segment_map
 
 
-def contours_segment(image: NDArray, levels: list):
-    "Build mask of source image using ski.measure's find_contours()."
-    contours: list = []
-    for level in levels:
-        contours.append(find_contours(image, level))
+def check_already_masked(galaxy_ix):
+    """
+    Check if there is a similar image and return a random index of one if there are any
+    """
+    is_masked: bool = False
+    index: int = 0
+    image: NDArray = DES_NO_SOURCE_DATA[galaxy_ix]
+    copies: list = []
+    for i in range(galaxy_ix):
+        previous_image = DES_NO_SOURCE_DATA[i]
+        if np.allclose(previous_image, image, atol=0.5):
+            is_masked = True
+            copies.append(i)
 
-    mask = 1 * polygon2mask((64, 64), contours[0][0])
-    if len(contours[0]) > 1:
-        mask = mask - 1 * polygon2mask((64, 64), contours[0][1])
-
-    return mask != 0
+    if copies:
+        index = np.random.choice(copies)
+    return is_masked, index
 
 
 def main():
@@ -98,110 +101,82 @@ def main():
 
     st.set_page_config(page_title="Lensing Masks", layout="wide")
 
-    explore = st.container()
-    with explore:
-        ix = st.number_input("Enter galaxy number", min_value=0, max_value=9999)
-        background_image = (
-            DES_NO_SOURCE_DATA[ix].transpose(1, 2, 0)[:, :, 1:4].sum(axis=2)
-        )
-        background_and_source = DES_DATA[ix].transpose(1, 2, 0)[:, :, 1:4].sum(axis=2)
-        source_image = background_and_source - background_image
+    ix = st.number_input("Enter galaxy number", min_value=0, max_value=9999)
+    background_image = DES_NO_SOURCE_DATA[ix].transpose(1, 2, 0)
+    lens_mask_fp: Path = LENSING_MASKS_DIR / f"{ix}_lens_mask.npy"
+    back_mask_fp: Path = LENSING_MASKS_DIR / f"{ix}_background_mask.npy"
 
-        lens_mask_fp: Path = LENSING_MASKS_DIR / f"{ix}_lens_mask.npy"
-        arcs_mask_fp: Path = LENSING_MASKS_DIR / f"{ix}_arcs_mask.npy"
-        back_mask_fp: Path = LENSING_MASKS_DIR / f"{ix}_background_mask.npy"
-
-        source_title, background_title = st.columns([2, 3])
-        with source_title:
-            _, title, _ = st.columns([2.57, 2.31, 1])
-            with title:
-                st.write(r"$\textsf{\Huge Source}$")
-
-        with background_title:
-            _, title, _ = st.columns([2, 2, 1])
-            with title:
-                st.write(r"$\textsf{\Huge Background}$")
-
-        arcs_col, source_col, lens_col, galaxy_col, background_col = st.columns(5)
-
-        with arcs_col:
-            threshold = st.slider(
-                "Select surface level",
-                min_value=0.11,
-                max_value=209.99,
-                key='arcs_th',
-                step=0.05,
-                value=45.9,
-            )
-            arcs_mask = contours_segment(source_image, [threshold])
-            # arcs_mask = segment_background(
-            #    source_image, thresh=threshold, exclude_pct=37.31
-            # )
-            # arcs_mask = arcs_mask._data > 0  # pylint: disable=protected-access
-            st.plotly_chart(px.imshow(arcs_mask, binary_string=True))
-            last_modified = datetime.fromtimestamp(arcs_mask_fp.stat().st_mtime)
-            st.write(f"Last modified: {last_modified.strftime('%d/%-m, %H:%M')}")
-
-        with source_col:
-            st.write("")
-            st.write("")
-            st.write("")
-            st.write("")
-            st.write("")
-            st.write("")
-            st.plotly_chart(px.imshow(source_image, binary_string=True))
-
-        with galaxy_col:
-            st.write("")
-            st.write("")
-            st.write("")
-            st.write("")
-            st.write("")
-            st.write("")
-            st.plotly_chart(px.imshow(background_image, binary_string=True))
-
+    edit_masks = st.container()
+    with edit_masks:
+        lens_col, galaxy_col, background_col = st.columns(3)
         with lens_col:
             threshold = st.slider(
                 "Select surface level",
                 min_value=-1.0,
-                max_value=50.0,
+                max_value=75.0,
                 value=14.14,
                 key='lens_th',
             )
             segmentation_map = segment_background(
-                background_image, threshold, exclude_pct=19.0
+                background_image.sum(axis=2), threshold, exclude_pct=19.0
             )
             lens_mask = keep_only_center(segmentation_map)
-            st.plotly_chart(px.imshow(lens_mask, binary_string=True))
-            last_modified = datetime.fromtimestamp(lens_mask_fp.stat().st_mtime)
-            st.write(f"Last modified: {last_modified.strftime('%d/%-m, %H:%M')}")
+            st.plotly_chart(px.imshow(lens_mask), theme=None)
 
         with background_col:
             threshold = st.slider(
                 "Select surface level",
                 min_value=-1.0,
-                max_value=50.0,
+                max_value=75.0,
                 value=1.9,
                 key='back_th',
             )
             segmentation_map = segment_background(
-                background_image, threshold, exclude_pct=19.0
+                background_image.sum(axis=2), threshold, exclude_pct=19.0
             )
             background_mask = remove_center_label(segmentation_map)
-            st.plotly_chart(px.imshow(background_mask, binary_string=True))
-            last_modified = datetime.fromtimestamp(back_mask_fp.stat().st_mtime)
-            st.write(f"Last modified: {last_modified.strftime('%d/%-m, %H:%M')}")
+            st.plotly_chart(px.imshow(background_mask), theme=None)
 
-        _, save_button_col, _ = st.columns([5, 5, 2])
-        with save_button_col:
-            if st.button("Save these masks", key='save_background'):
-                # np.save(back_mask_fp, background_mask)
-                # np.save(lens_mask_fp, lens_mask)
-                np.save(arcs_mask_fp, arcs_mask)
+        with galaxy_col:
+            _, save_button_col, _ = st.columns([2, 3, 1])
+            with save_button_col:
+                if st.button("Save these masks", key='save_background'):
+                    np.save(back_mask_fp, background_mask)
+                    np.save(lens_mask_fp, lens_mask)
+                    st.write(f"{back_mask_fp.stem} & {lens_mask_fp.stem} saved.")
+                else:
+                    st.write("")
+                    st.write("")
+            st.plotly_chart(px.imshow(background_image.sum(axis=2)), theme=None)
 
-        st.plotly_chart(
-            px.imshow(background_and_source, binary_string=True, width=800, height=800)
-        )
+    _, mask_ix = check_already_masked(ix)
+    _, msg_col, _ = st.columns([4.5, 5, 2])
+    with msg_col:
+        st.write(r":point_down: $\textsf{\LARGE Similar image found}$ :point_down:")
+
+    saved_lens_fp: Path = LENSING_MASKS_DIR / f"{mask_ix}_lens_mask.npy"
+    saved_back_fp: Path = LENSING_MASKS_DIR / f"{mask_ix}_background_mask.npy"
+    saved_back_mask = 1 * (np.load(saved_back_fp) > 0)  # avoid multi-labels
+    saved_lens_mask = 1 * (np.load(saved_lens_fp) > 0)  # avoid multi-labels
+
+    saved_masks_container = st.container()
+    with saved_masks_container:
+        saved_col1, saved_col2, saved_col3 = st.columns(3)
+        with saved_col1:
+            st.plotly_chart(px.imshow(saved_lens_mask), theme=None)
+        with saved_col2:
+            st.plotly_chart(px.imshow(background_image.sum(axis=2)), theme=None)
+        with saved_col3:
+            st.plotly_chart(px.imshow(saved_back_mask), theme=None)
+
+    _, use_saved_button, _ = st.columns([5, 5, 2])
+    with use_saved_button:
+        if st.button(f"Reuse saved masks {mask_ix}", key='use_saved'):
+            np.save(lens_mask_fp, saved_lens_mask)
+            np.save(back_mask_fp, saved_back_mask)
+            st.write(
+                f"Masks {mask_ix} saved as {back_mask_fp.stem} & {lens_mask_fp.stem}"
+            )
 
 
 if __name__ == "__main__":
