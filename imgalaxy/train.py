3ebb749305f2482b1fbe1131640147c712d29bd6
José Castagnino <castagninojose@gmail.com>
2024-11-26 14:26:50 -0300

    fk

--- imgalaxy/train.py
+++ imgalaxy/train.py
@@ -6,21 +6,21 @@ import wandb
 import yaml  # type: ignore
 from keras_unet_collection import models
 from tensorflow.keras.applications.vgg16 import preprocess_input
+from wandb.keras import WandbMetricsLogger
 
-from imgalaxy.cfg import PKG_PATH
+from imgalaxy.cfg import MODELS_DIR, PKG_PATH
 from imgalaxy.constants import IMAGE_SIZE, MASK, MIN_VOTE, NUM_EPOCHS
 from imgalaxy.helpers import check_augmented_images, evaluate_model
 from imgalaxy.unet import AugmentedSegmentationModel, GZ3DPipeline
 
 
 @click.command()
-@click.option(
-    "--loss", default="categorical_focal_crossentropy", show_default=True, help="Loss function."
-)
-@click.option("--batch-norm", default=True, show_default=True, help="Apply batch normalization.")
+# @click.option(
+#    "--loss", default="categorical_focal_crossentropy", show_default=True, help="Loss function."
+# )
 @click.option("--learning-rate", default=1e-02, show_default=True, help="Learning rate.")
 @click.option("--activation", default="ReLU", show_default=True, help="Activation function.")
-@click.option("--attention", default="add", show_default=True, help="Applied additive attention.")
+@click.option("--batch-norm", default=False, show_default=True, help="Apply batch normalization.")
 @click.option(
     "--atten-activation", default="ReLU", show_default=True, help="Non-linear attention activation."
 )
@@ -41,24 +41,30 @@ from imgalaxy.unet import AugmentedSegmentationModel, GZ3DPipeline
     show_default=True,
     help="Number of convolutional layers (after concatenation) per upsampling level/block.",
 )
+@click.option(
+    "--loss-gamma", default=0.2, show_default=True, help="Focusing parameter for loss function."
+)
+@click.option("--loss-smoothing", default=0.0, show_default=True, help="Label smoothing.")
+@click.option("--attention", default="add", show_default=True, help="Applied additive attention.")
 def train(
-    loss,
     learning_rate,
-    batch_norm,
     activation,
-    attention,
+    batch_norm,
     atten_activation,
     out_activation,
     pool,
     unpool,
     stack_num_down,
     stack_num_up,
+    loss_gamma,
+    loss_smoothing,
+    attention,
 ):
     with wandb.init(
         project="galaxy-segmentation-project",
         name=f"attention_unet_{MASK}",
         config={
-            'loss': loss,
+            #'loss': loss,
             'group': f"jose_{MASK}",
         },
     ):
@@ -75,7 +81,7 @@ def train(
             batch_norm=batch_norm,
             pool=pool,
             unpool=unpool,
-            backbone='vgg16',
+            backbone='VGG16',
             weights="imagenet",
             freeze_backbone=True,
             freeze_batch_norm=True,
@@ -111,8 +117,9 @@ def train(
         ds_test = ds_test.filter(lambda x: tf.reduce_max(x[MASK]) >= MIN_VOTE)
         train_batches = pipeline(ds_train)
         val_batches = pipeline(ds_val)
+        # loss = tf.keras.losses.SparseCategoricalCrossentropy(from_logits=False)
         loss = tf.keras.losses.CategoricalFocalCrossentropy(
-            alpha=[0.25, 0.75], gamma=0.1, label_smoothing=0.25, from_logits=False
+            alpha=[0.25, 0.75], gamma=loss_gamma, label_smoothing=loss_smoothing, from_logits=False
         )
         model.compile(
             loss=loss,
@@ -136,6 +143,7 @@ def train(
                 tf.keras.metrics.MeanIoU(
                     num_classes=2, sparse_y_true=False, sparse_y_pred=False, name="MeanIoU"
                 ),
+                tf.keras.losses.Dice(),
             ],
         )
         model_history = model.fit(
@@ -144,9 +152,18 @@ def train(
             # steps_per_epoch=STEPS_PER_EPOCH,
             # validation_steps=VALIDATION_STEPS,
             validation_data=val_batches,
+            callbacks=[
+                WandbMetricsLogger(),
+                tf.keras.callbacks.ModelCheckpoint(
+                    MODELS_DIR / f"best_att_spirals.keras",
+                    monitor='val_IoU_1',
+                    save_best_only=True,
+                    mode='max',
+                ),
+            ],
         )
-        check_augmented_images(ds_train)
-        evaluate_model(ds_test, model_history, num=3)
+        # check_augmented_images(ds_train)
+        # evaluate_model(ds_test, model_history, num=3)
 
 
 if __name__ == '__main__':
