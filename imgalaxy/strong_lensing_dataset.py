ea9ab921db53f3b0c3c0f6ada5b66356ebe5aec3
Jose Castagnino <jose@muttdata.ai>
2025-04-30 01:44:57 -0300

    add lensing dataset

--- imgalaxy/strong_lensing_dataset.py
+++ imgalaxy/strong_lensing_dataset.py
@@ -36,42 +36,40 @@ class LensingDataset(tfds.core.GeneratorBasedBuilder):
         # Replace with loading your real data
         images = np.load(path / 'images.npy')
 
-        backgrounds = np.load(path / 'background_masks.npy')
-        backgrounds = np.expand_dims(backgrounds, axis=-1)
+        background_masks = np.load(path / 'background_masks.npy')
+        background_masks = np.expand_dims(background_masks, axis=-1)
 
-        lenses = np.load(path / 'lens_masks.npy')
-        lenses = np.expand_dims(lenses, axis=-1)
+        lens_masks = np.load(path / 'lens_masks.npy')
+        lens_masks = np.expand_dims(lens_masks, axis=-1)
 
-        sources = np.load(path / 'source_masks.npy')
-        sources = np.expand_dims(sources, axis=-1)
+        source_masks = np.load(path / 'source_masks.npy')
+        source_masks = np.expand_dims(source_masks, axis=-1)
 
         for i in range(len(images)):
             yield i, {
                 'galaxy_id': f"{i}",
                 'image': images[i],
-                'background': backgrounds[i],
-                'lens': lenses[i],
-                'source': sources[i],
+                'background': background_masks[i],
+                'lens': lens_masks[i],
+                'source': source_masks[i],
             }
 
 
 def stack_images_and_masks(N: int = 9999, save_path: str = '/hdd/lensing-dataset/'):
     # Preallocate arrays
     images = np.zeros((N,) + (64, 64, 5), dtype=np.float32)
-    lens_masks = np.zeros((N,) + (64, 64), dtype=np.uint8)
-    background_masks = np.zeros((N,) + (64, 64), dtype=np.uint8)
-    source_masks = np.zeros((N,) + (64, 64), dtype=np.uint8)
+    lenses = np.zeros((N,) + (64, 64), dtype=np.uint8)
+    backgrounds = np.zeros((N,) + (64, 64), dtype=np.uint8)
+    sources = np.zeros((N,) + (64, 64), dtype=np.uint8)
 
     # Stack arrays by index number
     for idx in tqdm(range(N), desc="Stacking files"):
         images[idx] = DES_DATA[idx].transpose(1, 2, 0).astype(np.float32)
-        lens_masks[idx] = (np.load(LENSING_MASKS_DIR / f'{idx}_lens_mask.npy') > 0).astype(np.uint8)
-        background_masks[idx] = (
-            np.load(LENSING_MASKS_DIR / f'{idx}_background_mask.npy') > 0
-        ).astype(np.uint8)
-        source_masks[idx] = (np.load(LENSING_MASKS_DIR / f'{idx}_source_mask.npy') > 0).astype(
+        lenses[idx] = (np.load(LENSING_MASKS_DIR / f'{idx}_lens_mask.npy') > 0).astype(np.uint8)
+        backgrounds[idx] = (np.load(LENSING_MASKS_DIR / f'{idx}_background_mask.npy') > 0).astype(
             np.uint8
         )
+        sources[idx] = (np.load(LENSING_MASKS_DIR / f'{idx}_source_mask.npy') > 0).astype(np.uint8)
 
     output_dir = Path(save_path)
     output_dir.mkdir(parents=True, exist_ok=True)
@@ -79,11 +77,11 @@ def stack_images_and_masks(N: int = 9999, save_path: str = '/hdd/lensing-dataset
     print("Saving Galaxies...")
     np.save(output_dir / 'images.npy', images)
     print("Saving Lenses...")
-    np.save(output_dir / 'lens_masks.npy', lens_masks)
+    np.save(output_dir / 'lens_masks.npy', lenses)
     print("Saving Backgrounds...")
-    np.save(output_dir / 'background_masks.npy', background_masks)
+    np.save(output_dir / 'background_masks.npy', backgrounds)
     print("Saving Sources...")
-    np.save(output_dir / 'source_masks.npy', source_masks)
+    np.save(output_dir / 'source_masks.npy', sources)
 
 
 if __name__ == '__main__':
